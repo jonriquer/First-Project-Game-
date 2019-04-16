b276e1b16c84be016ce3beef8a1a579685965cbf
Jon Riquer <jonriquer@gmail.com>
2019-04-16 15:30:43 -0400

    added assets, organized code layout, began adding items to platforms

--- js/index.js
+++ js/index.js
@@ -21,11 +21,11 @@ var game = new Phaser.Game(config);
 let lava;
 
 function preload () {
-  //=========== Parallax BG =============
-  //= = = = = = = = = = = = = = = = = = =
-  this.load.image('floatingIsland', './../assets/platforms/leafy_ground05.png');
-  this.load.image('lavaTall', './../assets/tallLava.png');
-  this.load.image('ground', './assets/parallaxbg/01_ground.png');
+  this.load.spritesheet(
+    'dude', './assets/dino.png',
+    {frameWidth: 50, frameHeight: 60}
+  );
+  //=========== Background =============
   this.load.image('treesBushes', './assets/parallaxbg/02_treesandbushes.png');
   this.load.image('distantTrees', './assets/parallaxbg/03_distant_trees.png');
   this.load.image('bushes', './assets/parallaxbg/04_bushes.png');
@@ -36,30 +36,38 @@ function preload () {
   this.load.image('distantClouds1', './assets/parallaxbg/09_distant_clouds1.png');
   this.load.image('distantClouds2', './assets/parallaxbg/10_distant_clouds.png');
   this.load.image('tallBG', './../assets/tallBG.png');
-  this.load.image('background', './assets/parallaxbg/11_background.png');
-  this.load.image('platform', './assets/parallaxbg/platform.png');
-  //= = = = = = = = = = = = = = = = = = =
-  //========== Parallax BG End ===========
+  this.load.image('lavaTall', './../assets/tallLava.png');
+  //========== End Background ===========
 
-  //=========== Phaser Example ===========
-  //= = = = = = = = = = = = = = = = = = = 
+  //========== Main Structure ============
+  this.load.image('ground', './assets/parallaxbg/01_ground.png');
+  this.load.image('platform', './assets/parallaxbg/platform.png');
+  this.load.image('rockyTall', './../assets/rockyTall.png');
+  this.load.image('rockFloorLong', './../assets/platforms/rocky3.png');
+  this.load.image('rockFloor', './../assets/platforms/rocky02.png');
+  this.load.image('floatingIsland', './../assets/platforms/leafy_ground05.png');
+  //========= End Main Structure ===========
+
+  //=============== Extras =================
   this.load.image('banana', './assets/banana.png');
   this.load.image('bomb', './assets/asteroid.png');
-  this.load.spritesheet(
-      'dude', './assets/dino.png',
-      {frameWidth: 50, frameHeight: 60}
-  );
-  //= = = = = = = = = = = = = = = = = = = =
-  //========= Phaser Example End ==========
+  this.load.image('lookUp', './../assets/boardUp.png');
+  this.load.image('lookLeft', './../assets/boardLeft.png');
+  this.load.image('groundBottom', './../assets/groundBottom.png');
+  this.load.image('bigStone', './../assets/bigStone.png');
+  this.load.image('nest', './../assets/spikes and grass.png');
+  this.load.image('poisonNest', './../assets/poisonNest.png');
+  this.load.image('firstNest', './../assets/platforms/grassyFloor.png');
+  this.load.image('treasure', './../assets/treasureChest.png');
+  this.load.image('dangerSign', './../assets/danger.png');
+  //============== End Extras ================
 }
 
 function create () {
   
   //============ Parallax BG ===============
   //= = = = = = = = = = = = = = = = = = = =
-  let scrollBG = 
       this.tallBG = this.add.tileSprite(0,0,800,3000,'tallBG').setOrigin(0,0);
-      // this.add.image(400,300,'background');
       this.hugeClouds = this.add.tileSprite(400, 2650, 800, 600, 'hugeClouds');
       this.clouds = this.add.tileSprite(400, 2700, 800, 600,'clouds');
       this.distantClouds1 = this.add.tileSprite(400, 2700, 800, 600, 'distantClouds1');
@@ -69,13 +77,13 @@ function create () {
       this.bushes = this.add.tileSprite(400, 2700, 800, 600,'bushes');
       this.distantTrees = this.add.tileSprite(400, 2700, 800, 600, 'distantTrees');
       this.treesBushes = this.add.tileSprite(400, 2850, 1100, 900, 'treesBushes');
-      // this.ground = this.add.tileSprite(400, 570, 800, 60,'ground');
-
   //= = = = = = = = = = = = = = = = = = = = 
-  //========== End Parallax BG ============
+  //========== End Parallax BG =============
 
-  //============ Phaser Example ============
-  //= = = = = = = = Ground = = = = = = = = 
+
+
+  //=============== Platforms =========================
+  //= = = = = = = = = = = = = = = = = = = = = = = = = =
   platforms = this.physics.add.staticGroup();
   platforms.create(400, 3000, 'ground');
   platforms.create(295, 2500, 'platform');
@@ -83,28 +91,46 @@ function create () {
   platforms.create(70, 2650, 'platform');
   platforms.create(700, 2650, 'platform');
   platforms.create(700,2400, 'floatingIsland');
-  //= = = = = = = = = = = = = = = = = = = = 
-  //============ Phaser Example ============
-  
-  // this.floatingIsland = this.add.image(700,2400, "floatingIsland");
+  platforms.create(400,2250, 'floatingIsland');
+  platforms.create(100,2100, 'floatingIsland');
+  platforms.create(400,1950, 'floatingIsland');
+  platforms.create(700,1800, 'floatingIsland');
+  platforms.create(275,1650, 'firstNest');
+  platforms.create(30, 1475, 'rockFloor');
+  platforms.create(250, 1400, 'rockFloorLong');
+  platforms.create(250, 1300, 'rockFloorLong');
+  platforms.create(400, 1130, 'rockyTall');
+  platforms.create(30, 1100, 'rockFloor');
 
-  //============ Player ============
+    // Items on Platforms 
+    this.add.image(275,1680,'groundBottom');
+    this.add.image(200, 1600, 'lookLeft');
+    this.add.image(100, 1600, 'lookUp');
+    this.add.image(330,1580, 'dangerSign');
 
-  player = this.physics.add.sprite(200, 2770, 'dude');
+ 
+
+  //= = = = = = = = = = = = = = = = = = = = = = = = = = = = =  
+  //==================== End Platforms =======================
+ 
+
+
+  //============ Player ==============
+  //= = = = = = = = = = = = = = = = =
+  player = this.physics.add.sprite(10, 1400, 'dude');
   player.body.setGravityY(10)
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(player, lava);
   cursors = this.input.keyboard.createCursorKeys();
   
   //============ Camera ============
-  //= = = = = = = = = = = = = = = =
   var camera = this.cameras.main;
   camera.setViewport(0, 0, 800, 600);
   this.cameras.main.startFollow(player,true); 
   camera.setBounds(0,0,800,3000);
-  //= = = = = = = = = = = = = = = = 
   //=========== End Camera =========
 
+  //=========== Sprite Anim. ============
   player.setBounce(.2);
   this.physics.world.setBounds(0,0,800,3000)
   player.setCollideWorldBounds(true);
@@ -128,7 +154,12 @@ function create () {
       frameRate: 10,
       repeat: -1
   });
-  //=========== End Player =========
+  //=========== End Sprite Anim. =========
+
+  //= = = = = = = = = = = = = = = = = = = = = 
+  //=============== End Player ===============
+
+
 
   //=========== Items to Collect =============
   //= = = = = = = = = = = = = = = = = = = = = 
@@ -144,7 +175,7 @@ function create () {
   });
   this.physics.add.collider(bananas, platforms);
   this.physics.add.overlap(player, bananas, collectStar, null, this);
-
+  // Collection of Items
   function collectStar (player, banana) {
       banana.disableBody(true, true);
       score += 10;
@@ -162,8 +193,31 @@ function create () {
           bomb.setVelocity(Phaser.Math.Between(-200, 300), 20);
       }
   }
+
+  //========= Treasure Chests ===========
+    let treas = this.add.image(335, 1350, 'treasure');
+    let state = true;
+    let sizer = 1
+    setInterval(function () {
+        if (state){
+           sizer+=.01
+           treas.setScale(sizer)
+           if (sizer > 1.2){
+               state = false
+           }
+        } 
+        else {
+           sizer -=.01
+           if (sizer < 1){
+                 state = true
+           }
+        }
+    },20)
+  //========= End Treasure Chests =======
+
   //= = = = = = = = = = = = = = = = = = = = = =
   //========== End Items to Collect ===========
+
 
   //========== Scrore ==============
   var score = 0;
@@ -174,7 +228,12 @@ function create () {
   scoreText.setScrollFactor(0)
   //======= End Score ==============
 
-  //=========== Bombs ==============
+
+
+  //================ Deadly Ieams ==================
+  //= = = = = = = = = = = = = = = = = = = = = = = =
+
+  //=========== Asteroids ==============
   bombs = this.physics.add.group();
 
   this.physics.add.collider(bombs, platforms)
@@ -189,8 +248,39 @@ function create () {
 
       gameOver = true;
   }
-  //======== End Bombs ============
-  lava = this.lavaTile = this.add.tileSprite(0,2975,800,3000, 'lavaTall').setOrigin(0,0);
+  //======== End Asteroids ============
+
+  //============ Poison Nests ===========
+   this.add.image(355,1590,'nest');
+   poison = this.physics.add.group(); 
+   poison.create(350,1580, 'poisonNest');
+   this.physics.add.collider(poison, platforms);
+   this.physics.add.collider(player, poison, hitPoison, null, this);
+   function hitPoison (player, poison) {
+     this.add.text(450,100,'Game Over', {
+     font:'42px Arial black',
+     fill: '#000'
+     }).setScrollFactor(0);
+     this.physics.pause();
+
+     player.setTint(0xff0000);
+
+     player.anims.play('turn');
+
+     gameOver = true;
+   }
+  //========== End Poison Nests =========
+
+  //= = = = = = = = = = = = = = = = = = = = = = = =
+  //================ End Deadly Ieams ==============
+  
+
+  bigRocks = this.physics.add.group(); 
+  bigRocks.create(100,1580, 'bigStone');
+  this.physics.add.collider(bigRocks, platforms);
+  this.physics.add.collider(bigRocks, player);
+
+    lava = this.lavaTile = this.add.tileSprite(0,2975,800,3000, 'lavaTall').setOrigin(0,0);
 
 }
 
@@ -236,7 +326,7 @@ function update () {
     lava.y-=0.2;
   }
   
-  // moveLava();
+//   moveLava();
 
   if (player.y + player.height/2 > lava.y) {
     this.add.text(450,100,'Game Over', {
